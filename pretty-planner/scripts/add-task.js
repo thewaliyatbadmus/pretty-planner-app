f8493c06ef4fa537a4505c4287aa055c2f03bac4
Waliyat Badmus <w.badmus@alustudent.com>
2025-10-16 15:12:52 +0000

    menu button fix

--- pretty-planner/scripts/add-task.js
+++ pretty-planner/scripts/add-task.js
@@ -13,8 +13,9 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 
 menuBtn.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
+  menu.classList.toggle('show');
 });
+
 
 form.addEventListener('submit', e => {
   e.preventDefault();
